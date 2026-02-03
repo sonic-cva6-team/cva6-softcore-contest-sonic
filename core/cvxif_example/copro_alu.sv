587e6b20c76589cb328c855344bb6bd06689634b
Sonic <sonic.cva6@gmail.com>
2026-02-03 16:19:57 +0100

    Complex arithmetics coprocessor operations and software testbench
    Added complex operations instructions in coprocessor
    Modified cvxif-test.c to test the different instructions

--- core/cvxif_example/copro_alu.sv
+++ core/cvxif_example/copro_alu.sv
@@ -1,11 +1,5 @@
-// Copyright 2024 Thales DIS France SAS
-//
-// Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
-// You may obtain a copy of the License at https://solderpad.org/licenses/
-//
-// Original Author: Guillaume Chauvon
+// CVXIF coprocessor enabling complex arithmetic operations
+// Original Author: Martin ROUXEL - IMT Atlantique
 
 module copro_alu
   import cvxif_instr_pkg::*;
@@ -39,6 +33,10 @@ module copro_alu
   logic [4:0] rd_n, rd_q;
   logic we_n, we_q;
 
+  logic signed [15:0] a_re, a_im, b_re, b_im;
+  logic signed [31:0] re32, im32;
+  logic signed [31:0] ac, bd, ad, bc;
+
   assign result_o = result_q;
   assign hartid_o = hartid_q;
   assign id_o     = id_q;
@@ -46,8 +44,15 @@ module copro_alu
   assign rd_o     = rd_q;
   assign we_o     = we_q;
 
+  function automatic logic signed [15:0] sat16(input logic signed [31:0] x);
+    begin
+      if (x > 32'sd32767) sat16 = 16'sd32767;
+      else if (x < -32'sd32768) sat16 = -16'sd32768;
+      else sat16 = x[15:0];
+    end
+  endfunction
+
   always_comb begin
-    // defaults
     result_n = '0;
     hartid_n = hartid_i;
     id_n     = id_i;
@@ -55,15 +60,73 @@ module copro_alu
     rd_n     = '0;
     we_n     = 1'b0;
 
+    a_re     = $signed(registers_i[0][31:16]);
+    a_im     = $signed(registers_i[0][15:0]);
+    b_re     = $signed(registers_i[1][31:16]);
+    b_im     = $signed(registers_i[1][15:0]);
+
+    re32     = '0;
+    im32     = '0;
+    ac       = '0;
+    bd       = '0;
+    ad       = '0;
+    bc       = '0;
+
     case (opcode_i)
+      cvxif_instr_pkg::NOP: begin
+        result_n = '0;
+        valid_n  = 1'b1;
+        rd_n     = '0;
+        we_n     = 1'b0;
+      end
+
       cvxif_instr_pkg::ADD: begin
         result_n = registers_i[1] + registers_i[0];
         valid_n  = 1'b1;
         rd_n     = rd_i;
         we_n     = 1'b1;
       end
+
+      cvxif_instr_pkg::C_ADD: begin
+        re32     = $signed(a_re) + $signed(b_re);
+        im32     = $signed(a_im) + $signed(b_im);
+        result_n = {sat16(re32), sat16(im32)};
+        valid_n  = 1'b1;
+        rd_n     = rd_i;
+        we_n     = 1'b1;
+      end
+
+      cvxif_instr_pkg::C_SUB: begin
+        re32     = $signed(a_re) - $signed(b_re);
+        im32     = $signed(a_im) - $signed(b_im);
+        result_n = {sat16(re32), sat16(im32)};
+        valid_n  = 1'b1;
+        rd_n     = rd_i;
+        we_n     = 1'b1;
+      end
+
+      cvxif_instr_pkg::C_MUL: begin
+        ac       = $signed(a_re) * $signed(b_re);
+        bd       = $signed(a_im) * $signed(b_im);
+        ad       = $signed(a_re) * $signed(b_im);
+        bc       = $signed(a_im) * $signed(b_re);
+
+        re32     = ac - bd;
+        im32     = ad + bc;
+
+        result_n = {re32[31:16], im32[31:16]};
+        valid_n  = 1'b1;
+        rd_n     = rd_i;
+        we_n     = 1'b1;
+      end
+
       default: begin
-        // keep defaults
+        result_n = '0;
+        hartid_n = '0;
+        id_n     = '0;
+        valid_n  = '0;
+        rd_n     = '0;
+        we_n     = '0;
       end
     endcase
   end
--- core/cvxif_example/include/cvxif_instr_pkg.sv
+++ core/cvxif_example/include/cvxif_instr_pkg.sv
@@ -1,23 +1,18 @@
-// Copyright 2021 Thales DIS design services SAS
-//
-// Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
-// You may obtain a copy of the License at https://solderpad.org/licenses/
-//
-// Original Author: Guillaume Chauvon (guillaume.chauvon@thalesgroup.com)
-
 package cvxif_instr_pkg;
 
   typedef enum logic [3:0] {
     ILLEGAL = 4'b0000,
-    ADD     = 4'b0010
+    NOP     = 4'b0001,
+    ADD     = 4'b0010,
+    C_ADD   = 4'b0011,
+    C_SUB   = 4'b0100,
+    C_MUL   = 4'b0101
   } opcode_t;
 
   typedef struct packed {
     logic accept;
-    logic writeback;  // TODO depends on dualwrite
-    logic [2:0] register_read;  // TODO Nr read ports
+    logic writeback;
+    logic [2:0] register_read;
   } issue_resp_t;
 
   typedef struct packed {
@@ -38,27 +33,55 @@ package cvxif_instr_pkg;
     compressed_resp_t resp;
   } copro_compressed_resp_t;
 
-  // Only 1 RISCV instruction for Coprocessor
-  parameter int unsigned NbInstr = 1;
+  // NOP + ADD + C_ADD + C_SUB + C_MUL
+  parameter int unsigned NbInstr = 5;
   parameter copro_issue_resp_t CoproInstr[NbInstr] = '{
       '{
-          // Custom Add : cus_add rd, rs1, rs2
-          instr: 32'b00000_00_00000_00000_0_01_00000_1111011,  // custom3 opcode
-          mask:  32'b11111_11_00000_00000_1_11_00000_1111111,
-          resp:  '{accept : 1'b1, writeback : 1'b1, register_read : {1'b0, 1'b1, 1'b1}},
+          // Custom NOP (funct3=0)
+          instr:
+          32'b00000_00_00000_00000_0_00_00000_1111011,
+          mask: 32'b11111_11_00000_00000_1_11_00000_1111111,
+          resp: '{accept : 1'b1, writeback : 1'b0, register_read : {1'b0, 1'b0, 1'b0}},
+          opcode: NOP
+      },
+      '{
+          // Custom ADD (funct3=1)
+          instr:
+          32'b00000_00_00000_00000_0_01_00000_1111011,
+          mask: 32'b11111_11_00000_00000_1_11_00000_1111111,
+          resp: '{accept : 1'b1, writeback : 1'b1, register_read : {1'b0, 1'b1, 1'b1}},
           opcode: ADD
+      },
+      '{
+          // Complex ADD (funct3=2)
+          instr:
+          32'b00000_00_00000_00000_0_10_00000_1111011,
+          mask: 32'b11111_11_00000_00000_1_11_00000_1111111,
+          resp: '{accept : 1'b1, writeback : 1'b1, register_read : {1'b0, 1'b1, 1'b1}},
+          opcode: C_ADD
+      },
+      '{
+          // Complex SUB (funct3=3)
+          instr:
+          32'b00000_00_00000_00000_0_11_00000_1111011,
+          mask: 32'b11111_11_00000_00000_1_11_00000_1111111,
+          resp: '{accept : 1'b1, writeback : 1'b1, register_read : {1'b0, 1'b1, 1'b1}},
+          opcode: C_SUB
+      },
+      '{
+          // Complex MUL (funct3=4)
+          instr:
+          32'b00000_00_00000_00000_1_00_00000_1111011,
+          mask: 32'b11111_11_00000_00000_1_11_00000_1111111,
+          resp: '{accept : 1'b1, writeback : 1'b1, register_read : {1'b0, 1'b1, 1'b1}},
+          opcode: C_MUL
       }
   };
 
-  // Dummy compressed instruction set
+  // Dummy compressed instructions
   parameter int unsigned NbCompInstr = 1;
   parameter copro_compressed_resp_t CoproCompInstr[NbCompInstr] = '{
-    '{
-      instr : 16'b0,
-      mask  : 16'b0,
-      resp  : '{accept : 1'b0, instr : 32'b0}
-    }
+      '{instr : 16'b0, mask  : 16'b0, resp  : '{accept : 1'b0, instr : 32'b0}}
   };
-
 
 endpackage
